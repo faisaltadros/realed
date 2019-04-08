f474e6d026c39290f088891a2ab166e4e632bfd5
Faisal <faisal@rotatestudios.com>
2019-04-08 18:38:47 -0500

    update mongoose connect again

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    flash       = require("connect-flash"),
-    passport    = require("passport"),
-    LocalStrategy = require("passport-local"),
-    methodOverride = require("method-override"),
-    Campground  = require("./models/campground"),
-    Comment     = require("./models/comment"),
-    User        = require("./models/user"),
-    seedDB      = require("./seeds")
-    
+var express = require("express"),
+   app = express(),
+   bodyParser = require("body-parser"),
+   mongoose = require("mongoose"),
+   flash = require("connect-flash"),
+   passport = require("passport"),
+   LocalStrategy = require("passport-local"),
+   methodOverride = require("method-override"),
+   Campground = require("./models/campground"),
+   Comment = require("./models/comment"),
+   User = require("./models/user"),
+   seedDB = require("./seeds")
+
 //requiring routes
-var commentRoutes    = require("./routes/comments"),
-    campgroundRoutes = require("./routes/campgrounds"),
-    indexRoutes      = require("./routes/index")
-    
+var commentRoutes = require("./routes/comments"),
+   campgroundRoutes = require("./routes/campgrounds"),
+   indexRoutes = require("./routes/index")
+
 // mongoose.connect("mongodb://localhost/yelp_camp_v10");
-mongoose.connect("mongodb+srv://faisalt5:cartman55%@cluster0-ub6wn.mongodb.net/test?retryWrites=true");
+// mongoose.connect("mongodb+srv://faisalt5:cartman55%@cluster0-ub6wn.mongodb.net/test?retryWrites=true");
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://faisal1234:faisal1234@cluster0-ub6wn.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 
-app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
@@ -30,9 +40,9 @@ app.use(flash());
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
-    secret: "Once again Rusty wins cutest dog!",
-    resave: false,
-    saveUninitialized: false
+   secret: "Once again Rusty wins cutest dog!",
+   resave: false,
+   saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -40,7 +50,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
    res.locals.currentUser = req.user;
    res.locals.error = req.flash("error");
    res.locals.success = req.flash("success");
@@ -52,6 +62,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 var port = process.env.PORT || 3000;
-app.listen(port, process.env.IP, function(){
+app.listen(port, process.env.IP, function () {
    console.log("The YelpCamp Server Has Started!");
 });
